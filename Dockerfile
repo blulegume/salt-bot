9e39ab8db26edc7e2ec92584690c1ad9cd050d12
blulegume <mngreenforest@gmail.com>
2020-11-27 23:44:35 -0500

    it works

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.8-slim
+
+WORKDIR /code
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY src/ .
+
+ENV SUMMON_COMMAND ${SUMMON_COMMAND}
+ENV EMOJI ${EMOJI}
+ENV API_KEY ${API_KEY}
+ENV SUMMON_ROLE ${SUMMON_ROLE}
+
+CMD [  "python", "./main.py"]
--- src/main.py
+++ src/main.py
@@ -4,86 +4,97 @@ from os import getenv
 import discord
 from discord.ext import commands
 
-SUMMON_COMMAND= getenv('SUMMON_COMMAND') or '*:salt:'
-EMOJI = getenv('EMOJI_NAME') or ':salt:'
-API_KEY = getenv('API_KEY') or ''
+SUMMON_COMMAND= getenv('SUMMON_COMMAND') or '🧂'
+EMOJI = getenv('EMOJI_NAME') or '🧂'
+API_KEY = getenv('API_KEY') or 'NO API KEY'
 SUMMON_ROLE = getenv('SUMMON_ROLE') or None
 
-COMMANDS = '''
+COMMANDS = f'''
+:salt: :salt: :salt:
+```
 Commands:
-    :salt: :salt: :salt:
-    <user...>           Add user(s) to the list of saltees
-    unsalt <user...>    Remove user(s) from the list of saltees
-    h | help            Get help with commands
+    🧂 <user...>            Add user(s) to the list of saltees
+    unsalt <user...>        Remove user(s) from the list of saltees
+    h                       Get help with commands
 
     You must have the role of {SUMMON_ROLE} to do this
+```
 '''
 
-members = load('members.json')
-client = discord.Client()
+member_store = []
+with open('members.json', 'r') as f:
+    member_store = load(f)
+
 bot = commands.Bot(command_prefix=SUMMON_COMMAND)
 
-@client.event
+@bot.listen('on_ready')
 async def on_ready():
     print('Beep boop I have started')
-    print(members)
+    print(member_store)
+    
 
-@client.event
+@bot.listen('on_message')
 async def on_message(message):
     if message.channel.type == "private":
         await message.channel.send(commands)
         await react(message)
-    # if sender is a member, react to message
+
     if should_salt(message):
-        await react(message)
-    if message.content.strip().startswith(SUMMON_COMMAND):
-        on_summon(message)
-
-async def on_summon(message):
-    content = message.content.split()
-    if len(content) < 2:
-        await react(message)
-
-    command = content[1]
-    if command == 'h' or command == ['help']:
-        await message.channel.send(COMMANDS)
-        return
-    if can_summon(message.author):
-        member_ids = map(lambda member: member.id, members)
-        if command = "unsalt":
-            unsalt(member_ids)
-        else:
-            add_members(member_ids)
-    else: 
-        message.channel.send(f'You need the role of {SUMMON_ROLE} to do that, dummy')
         await react(message)
 
 async def react(message):
     await message.add_reaction(EMOJI)
-    print(f'I just reacted to a message : {message.content}')
+
+def should_salt(message):
+    mention_ids = map(lambda mention: mention.id, message.mentions)
+    return  (message.author.id in member_store) # or (bot and bot.id in mention_ids)
+
+
+@bot.command()
+async def h(ctx):
+    await ctx.send(COMMANDS)
+
+@bot.command(name="🧂")
+async def salt(ctx, members: commands.Greedy[discord.Member]):
+    if can_summon(ctx.author):
+        member_ids = list(map(lambda member: member.id, members))
+        add_members(member_ids)
+    else: 
+        ctx.send(f'You need the role of {SUMMON_ROLE} to do that, dummy')
+        await react(ctx.message)
+
+@bot.command()  
+async def unsalt(ctx, members: commands.Greedy[discord.Member]):
+    if can_summon(ctx.author):
+        member_ids = list(map(lambda member: member.id, members))
+        remove_members(member_ids)
+        
+    else:
+        ctx.send(f'You need the role of {SUMMON_ROLE} to do that, dummy')
+        await react(ctx.message)
 
 def can_summon(author):
     return (not SUMMON_ROLE) or SUMMON_ROLE in filter(lambda role: role.name, author.roles)
 
-def should_salt(message):
-    mentions_ids = map(lambda mention: mention.id, message.mentions)
-    return  (message.author.id in members) or (client and client.id in mention_ids)
-
 def add_members(member_ids):
     for id in member_ids:
-        if id not in members:
-            members.append(id)
-    update_member_file()
-    
-def unsalt(member_ids):
-    members = [member for member in members if member not in member_ids]
+        if id not in member_store:
+            member_store.append(id)
+        else:
+            print(f'{id} is i n member store {member_store}')
     update_member_file()
 
 def update_member_file():
     with open('members.json', 'w') as f:
-        dump(members, f)
+        dump(member_store, f)
 
-client.run(API_KEY)
+def remove_members(member_ids):
+    for id in member_ids:
+        if id in member_store:
+            member_store.remove(id)
+    update_member_file()
+
+bot.run(API_KEY)
 
     
 
